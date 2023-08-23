bc5f12abd192bf38a6d1b543fefbdeef7ad75de0
ishimaru <takato1430@outlook.jp>
2023-08-23 16:38:06 +0900

    no message

--- Assets/Scripts/MyScript/AndroidManager.cs
+++ Assets/Scripts/MyScript/AndroidManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AndroidManager : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class AndroidManager : MonoBehaviour
     ServerManager serverManager;
 
     [SerializeField] Camera androidCamera;
+    [SerializeField] Text posMessage;
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +24,21 @@ public class AndroidManager : MonoBehaviour
         SendCameraPos();
     }
 
+    /*
+         [SerializeField] OVRSkeleton skeleton;
+
+      transform.position = skeleton.Bones[(int)OVRSkeleton.BoneId.Hand_IndexTip].Transform.position;
+        positionText.text = "position: x-"+transform.position.x+
+                            ", y-"+transform.position.y+", z-"+transform.position.z;
+      
+      
+     */
+
     private void SendCameraPos()
     {
         clientManager.SendVector(androidCamera.transform.position);
+        posMessage.text = "This.CameraPos: x=" +androidCamera.transform.position.x+
+                          "\ny=" + androidCamera.transform.position.y +
+                          "\nz=" + androidCamera.transform.position.z;
     }
 }
